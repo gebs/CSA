a5d5b56c948c9660084a80b9a994bac0853c5c3b
psychonoob <adrian.gebs@stud.hslu.ch>
2016-09-27 20:23:31 +0200

    Uebung 3 Finished

--- Uebung3/Uebung3/Form1.Designer.cs
+++ Uebung3/Uebung3/Form1.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.checkBoxNegative = new System.Windows.Forms.CheckBox();
+            this.textBoxAbsValue = new System.Windows.Forms.TextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.numericUpDownValue = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBoxNegative
+            // 
+            this.checkBoxNegative.AutoSize = true;
+            this.checkBoxNegative.Enabled = false;
+            this.checkBoxNegative.Location = new System.Drawing.Point(53, 27);
+            this.checkBoxNegative.Name = "checkBoxNegative";
+            this.checkBoxNegative.Size = new System.Drawing.Size(58, 21);
+            this.checkBoxNegative.TabIndex = 0;
+            this.checkBoxNegative.Text = "neg.";
+            this.checkBoxNegative.UseVisualStyleBackColor = true;
+            // 
+            // textBoxAbsValue
+            // 
+            this.textBoxAbsValue.Location = new System.Drawing.Point(162, 25);
+            this.textBoxAbsValue.Name = "textBoxAbsValue";
+            this.textBoxAbsValue.ReadOnly = true;
+            this.textBoxAbsValue.Size = new System.Drawing.Size(120, 22);
+            this.textBoxAbsValue.TabIndex = 1;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(53, 77);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownValue
+            // 
+            this.numericUpDownValue.Location = new System.Drawing.Point(162, 77);
+            this.numericUpDownValue.Minimum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownValue.Name = "numericUpDownValue";
+            this.numericUpDownValue.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownValue.TabIndex = 3;
+            this.numericUpDownValue.ThousandsSeparator = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 142);
+            this.Controls.Add(this.numericUpDownValue);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.textBoxAbsValue);
+            this.Controls.Add(this.checkBoxNegative);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownValue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxNegative;
+        private System.Windows.Forms.TextBox textBoxAbsValue;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.NumericUpDown numericUpDownValue;
     }
 }
 
--- Uebung3/Uebung3/Form1.cs
+++ Uebung3/Uebung3/Form1.cs
@@ -15,6 +15,24 @@ namespace Uebung3
         public Form1()
         {
             InitializeComponent();
+
+            this.numericUpDownValue.ValueChanged += NumericUpDownValue_ValueChanged;
+            this.buttonClear.Click += ButtonClear_Click;
+            this.textBoxAbsValue.Text = 0.ToString();
+            this.checkBoxNegative.Checked = false;
+        }
+
+        private void ButtonClear_Click(object sender, EventArgs e)
+        {
+            this.textBoxAbsValue.Text = "";
+            this.checkBoxNegative.Checked = false;
+            this.numericUpDownValue.Value = 0;
+        }
+
+        private void NumericUpDownValue_ValueChanged(object sender, EventArgs e)
+        {
+            this.checkBoxNegative.Checked = (numericUpDownValue.Value)  < 0;
+            this.textBoxAbsValue.Text = Math.Abs(numericUpDownValue.Value).ToString();
         }
     }
 }
