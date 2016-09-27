3e3a67338ae2a6d3e859285d0d5f8c638b126b17
Adrian Gebs <Adrian Gebs>
2016-09-27 19:10:50 +0200

    ASDF

---
+++ Uebung2/Uebung2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung2
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            StringList sl = new StringList();
+            sl.Size = 5;
+
+            sl.Data[1] = "Some Value";
+            sl.Data[3] = "Another Value";
+
+            foreach (string s in sl.Data)
+            {
+                Console.WriteLine($"entry: {s}");
+            }
+            Console.WriteLine("");
+
+            sl[4] = "Something else";
+            sl[0] = $"{sl[1]}, {sl[3]}";
+
+            for (int i = 0; i < sl.Size; i++)
+            {
+                Console.WriteLine($"entry:[{i}]: {sl[i]}");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Uebung2/Uebung2/StringList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung2
+{
+    public class StringList
+    {
+        private int size;
+        public int Size
+        {
+            get { return size; }
+            set
+            {
+                size = value;
+                Data = new string[size];
+            }
+        }
+
+        
+        public string[] Data { get; set; }
+        public string this[int index]
+        {
+            get { return Data?[index] ?? ""; }
+            set { Data[index] = value; }
+        }
+        public StringList()
+        {
+          
+        }
+    }
+}
