75389fc50a8dabd861af8edbf9db659bd25d0cbd
psychonoob <adrian.gebs@stud.hslu.ch>
2016-11-15 20:03:53 +0100

    -

---
+++ Robot/TestDrive/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿using RobotCtrl;
+using RobotView;
+
+namespace TestDrive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonHalt = new System.Windows.Forms.Button();
+            this.driveView1 = new RobotView.DriveView();
+            this.consoleView1 = new RobotView.ConsoleView();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.commonRunParameters1 = new RobotView.CommonRunParameters();
+            this.runlineView = new RobotView.RunLineView();
+            this.runturnView = new RobotView.RunTurnView();
+            this.runarcView = new RobotView.RunArcView();
+            this.radarView = new RobotView.RadarView();
+            this.SuspendLayout();
+            // 
+            // buttonHalt
+            // 
+            this.buttonHalt.Location = new System.Drawing.Point(248, 335);
+            this.buttonHalt.Name = "buttonHalt";
+            this.buttonHalt.Size = new System.Drawing.Size(70, 40);
+            this.buttonHalt.TabIndex = 0;
+            this.buttonHalt.Text = "Halt";
+       
+            // 
+            // driveView1
+            // 
+            this.driveView1.Drive = null;
+            this.driveView1.Location = new System.Drawing.Point(12, 4);
+            this.driveView1.Name = "driveView1";
+            this.driveView1.Size = new System.Drawing.Size(292, 315);
+            this.driveView1.TabIndex = 1;
+            // 
+            // consoleView1
+            // 
+            this.consoleView1.BackColor = System.Drawing.Color.Black;
+            this.consoleView1.Console = null;
+            this.consoleView1.Location = new System.Drawing.Point(12, 347);
+            this.consoleView1.Name = "consoleView1";
+            this.consoleView1.Size = new System.Drawing.Size(209, 46);
+            this.consoleView1.TabIndex = 2;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(248, 381);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(70, 40);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Stop";
+           
+            // 
+            // commonRunParameters1
+            // 
+            this.commonRunParameters1.Acceleration = 0.3F;
+            this.commonRunParameters1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.commonRunParameters1.Location = new System.Drawing.Point(350, 3);
+            this.commonRunParameters1.Name = "commonRunParameters1";
+            this.commonRunParameters1.Size = new System.Drawing.Size(351, 84);
+            this.commonRunParameters1.Speed = 0.5F;
+            this.commonRunParameters1.TabIndex = 4;
+            // 
+            // runlineView
+            // 
+            this.runlineView.Acceleration = 0F;
+            this.runlineView.Drive = null;
+            this.runlineView.Location = new System.Drawing.Point(350, 94);
+            this.runlineView.Name = "runlineView";
+            this.runlineView.Size = new System.Drawing.Size(351, 56);
+            this.runlineView.Speed = 0F;
+            this.runlineView.TabIndex = 5;
+            // 
+            // runturnView
+            // 
+            this.runturnView.Acceleration = 0F;
+            this.runturnView.Drive = null;
+            this.runturnView.Location = new System.Drawing.Point(350, 156);
+            this.runturnView.Name = "runturnView";
+            this.runturnView.Size = new System.Drawing.Size(351, 55);
+            this.runturnView.Speed = 0F;
+            this.runturnView.TabIndex = 6;
+            // 
+            // runarcView
+            // 
+            this.runarcView.Acceleration = 0F;
+            this.runarcView.Drive = null;
+            this.runarcView.Location = new System.Drawing.Point(350, 217);
+            this.runarcView.Name = "runarcView";
+            this.runarcView.Size = new System.Drawing.Size(351, 107);
+            this.runarcView.Speed = 0F;
+            this.runarcView.TabIndex = 7;
+            // 
+            // radarView
+            // 
+            this.radarView.Location = new System.Drawing.Point(350, 335);
+            this.radarView.Name = "radarView";
+            this.radarView.Size = new System.Drawing.Size(351, 52);
+            this.radarView.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(798, 435);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonHalt);
+            this.Controls.Add(this.driveView1);
+            this.Controls.Add(this.consoleView1);
+            this.Controls.Add(this.commonRunParameters1);
+            this.Controls.Add(this.runlineView);
+            this.Controls.Add(this.runturnView);
+            this.Controls.Add(this.runarcView);
+            this.Controls.Add(this.radarView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonHalt;
+        private DriveView driveView1;
+        private ConsoleView consoleView1;
+        private CommonRunParameters commonRunParameters1;
+        private System.Windows.Forms.Button buttonStop;
+        private RunLineView runlineView;
+        private RunTurnView runturnView;
+        private RunArcView runarcView;
+        private RadarView radarView;
+    }
+}
+
---
+++ Robot/TestDrive/Form1.cs
@@ -0,0 +1,91 @@
+﻿
+using RobotCtrl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TestDrive
+{
+    public partial class Form1 : Form
+    {
+        #region members
+        private Robot robot;
+        #endregion
+        
+        #region constructor & destructor
+        public Form1()
+        {
+            InitializeComponent();
+
+            robot = new Robot();        // neuen Roboter erstellen
+            robot.Drive.Power = true;   // Stromversorgung der Motoren (im DriveCtrl) einschalten
+
+            runlineView.Drive = robot.Drive;
+            runturnView.Drive = robot.Drive;
+            runarcView.Drive = robot.Drive;
+            driveView1.Drive = robot.Drive; // DriveView benötigt Drive-Objekt zur Visualisierung
+            
+            
+            Init();
+            radarView.Radar = robot.Radar;
+            consoleView1.Console = robot.RobotConsole;
+
+            commonRunParameters1.AccelerationChanged += AccelerationChanged;
+            commonRunParameters1.SpeedChanged += SpeedChanged;
+
+            SpeedChanged(null, EventArgs.Empty); // Default Wert setzen
+            AccelerationChanged(null, EventArgs.Empty); // Default Wert setzen
+            this.buttonHalt.Click += ButtonHalt_Click;
+            this.buttonStop.Click += ButtonStop_Click;
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            robot.Drive.Stop();
+        }
+
+        private void ButtonHalt_Click(object sender, EventArgs e)
+        {
+            robot.Drive.Halt();
+        }
+        #endregion
+
+        private void Init()
+        {
+            runlineView.Speed = commonRunParameters1.Speed;
+            runlineView.Acceleration = commonRunParameters1.Acceleration;
+            runturnView.Speed = commonRunParameters1.Speed;
+            runturnView.Acceleration = commonRunParameters1.Acceleration;
+            runarcView.Speed = commonRunParameters1.Speed;
+            runarcView.Acceleration = commonRunParameters1.Acceleration;
+        }
+
+        #region methods
+        private void SpeedChanged(object sender, EventArgs e)
+        {
+            runlineView.Speed = commonRunParameters1.Speed;
+            runturnView.Speed = commonRunParameters1.Speed;
+            runarcView.Speed = commonRunParameters1.Speed;
+          
+
+        }
+        private void AccelerationChanged(object sender, EventArgs e)
+        {
+            runlineView.Acceleration = commonRunParameters1.Acceleration;
+            runturnView.Acceleration = commonRunParameters1.Acceleration;
+            runarcView.Acceleration = commonRunParameters1.Acceleration;
+        }
+
+
+
+        #endregion
+
+    }
+}
